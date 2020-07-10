0a272aaff91c1017806c64e753650a1cb2897d8e
walter <walter@dipu.com>
2020-07-10 11:16:20 +0200

    v1

---
+++ src/BugTracker/BugTracker/BugTracker.Droid/obj/Debug/90/android/src/crc64fef93827e73f2891/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64fef93827e73f2891;
+
+
+public class MainActivity
+	extends crc64122dcf5ad656876d.ApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("BugTracker.Droid.MainActivity, BugTracker.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BugTracker.Droid.MainActivity, BugTracker.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BugTracker.Droid.MainActivity, BugTracker.Droid", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ src/BugTracker/BugTracker/BugTracker.Wasm/bin/Debug/netstandard2.0/dist/service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('managed-65f90985a89d7873129e67a81e046e9c2b07e327').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed-65f90985a89d7873129e67a81e046e9c2b07e327
